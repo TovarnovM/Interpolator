f7278f858a6b684f704e3eefe7da49940744b0c8
TovarnovM <misha198921rip@gmail.com>
2016-03-16 12:24:14 +0300

    Begin

---
+++ InterpSolution/InterpApp/Interp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolator
+{
+    public interface IInterpElem
+    {
+        double GetV(params double[] t);
+        
+    }
+    class InterpDouble : IInterpElem
+    {
+        public double Value { get; set; }
+        double IInterpElem.GetV(params double[] t)
+        {
+            return Value;
+        }
+    }
+
+    public class Interp<T>:IInterpElem where T :IInterpElem
+    {
+        private SortedList<double, T> _data = new SortedList<double, T>() ;
+
+        double IInterpElem.GetV(params double[] t)
+        {
+            if(t.Length > 0)
+            {
+                return 0;
+                //Короч запилить
+            }
+        }
+    }
+}
