d4453b0fc8b5f2f7fd72c0f7b5067f47085038dd
TovarnovM <misha198921rip@gmail.com>
2016-05-22 23:13:09 +0300

    +3D vect

---
+++ InterpSolution/My3DVect/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My3DVect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
