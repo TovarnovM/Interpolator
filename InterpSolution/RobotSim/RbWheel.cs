ed12ce12caaea6152937353c8086eeb51c5212c5
Михаил Товарнов <misha198921rip@gmail.com>
2016-11-29 14:02:30 +0300

    ww

---
+++ InterpSolution/RobotSim/RbWheel.cs
@@ -0,0 +1,39 @@
+﻿using Sharp3D.Math.Core;
+using SimpleIntegrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSim {
+    public class RbWheel: Orient3D {
+        public double Ix { get; set; }
+        public double Mass { get; set; }
+        public double Mx { get; set; }
+
+        public double R0;
+        public double X0;
+        public double kR;
+        public double muR;
+        public double kX;
+        public double muX;
+
+        public int n_shag;
+        public double Rmax;
+
+        public double OmegaX { get; set; }
+        public IScnPrm pOmegaX { get; set; }
+
+        public double Betta { get; set; }
+        public IScnPrm pBetta { get; set; }
+
+        public RbWheel() {
+            AddDiffPropToParam(pBetta,pOmegaX);
+        }
+
+        public Vector3D GetWorldPos(int spikeIndex) {
+
+        }
+    }
+}
--- InterpSolution/RobotSim/RobotDynamics.cs
+++ InterpSolution/RobotSim/RobotDynamics.cs
@@ -14,7 +14,7 @@ namespace RobotSim {
 
 
 
-        public List<RobotWheel> Wheels { get; set; } = new List<RobotWheel>();
+        //public List<RobotWheel> Wheels { get; set; } = new List<RobotWheel>();
         const string DEFNAME = "Robot";
         public RobotDynamics(string name = DEFNAME) :base(name) {
             l = 0.2;
@@ -36,79 +36,4 @@ namespace RobotSim {
 
     }
 
-    public class RobotWheel: MaterialObjectNewton {
-        public Vector3D Center;
-        public Vector3D CenterVel;
-        public Vector3D OXNorm;
-        public IOrient3D Body { get; set; }
-
-        public ForceCenter ConnectionForce { get; set; } 
-        public ForceCenter ConnectionVelForce { get; set; }
-        public ForceCenter ConnectionMoment { get; set; }
-
-        public void SynchMassGeometry() {
-            Mass.Ix = Mass.Value * Radius * Radius * 0.5;
-            Mass.Iy = 0.25 * Mass.Value * Radius * Radius + Mass.Value * H * H / 12d;
-            Mass.Iz = Mass.Iy;
-        }
-
-        public double Radius { get; set; } = 0.05;
-        public double H { get; set; } = 0.01;
-
-        public double Kx { get; set; } = 1000;
-        public double Mu_x { get; set; } = 500;
-
-        public double Kyz { get; set; } = 100;
-        public double Mu_yz { get; set; } = 100;
-
-        const string DEFNAME = "Wheel";
-        public RobotWheel(IOrient3D body, Vector3D center,Vector3D centerVel,Vector3D oxNorm, string name = DEFNAME):base(center, name) {
-            Body = body;
-            Center = center;
-            CenterVel = centerVel;
-            OXNorm = oxNorm;
-
-            Mass.Value = 0.05;
-            SynchMassGeometry();
-
-            RotateOXtoVec(OXNorm);
-
-            ConnectionForce = new ForceCenter(0, null,null,"ConnectionForce");
-            AddChild(ConnectionForce);
-
-            ConnectionVelForce = new ForceCenter(0,null,null,"ConnectionVelForce");
-            AddChild(ConnectionVelForce);
-
-            SynchMeBefore += ConnForcesSynch;
-
-
-        }
-
-        public void ConnForcesSynch(double t) {
-            var r = Vec3D - Body.WorldTransform*Center;
-            var n = Body.WorldTransform * OXNorm;
-            var rx = (r*n)*n;
-            var ryz = r - rx;
-
-            var kFx = - rx * Kx;
-            var kFyz = -ryz * Kyz;
-            var kF = kFx + kFyz;
-
-            ConnectionForce.Value = kF.GetLength();
-            ConnectionForce.Direction.Vec3D = kF.Norm;
-
-            var votn = Vel.Vec3D - Body.WorldTransform * CenterVel;
-            var votnx = (votn * n) * n;
-            var votnyz = votn - votnx;
-
-            var muFx = -votnx * Mu_x;
-            var muFyz = -votnyz * Mu_yz;
-            var muF = muFx + muFyz;
-
-            ConnectionVelForce.Value = muF.GetLength();
-            ConnectionVelForce.Direction.Vec3D = muF.Dir;
-
-        }
-
-    }
 }
