77f61701911ad4bb0f19e20a315a2feeb0b57319
User <User@USER-PK>
2017-09-06 19:28:22 +0300

    WWW

---
+++ InterpSolution/MeetingPro/Mis.cs
@@ -0,0 +1,15 @@
+﻿using Interpolator;
+using SimpleIntegrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingPro {
+    public class Mis: MaterialObjectNewton {
+        public Dictionary<string, Interp2D> Graphs { get; set; }
+
+        
+    }
+}
