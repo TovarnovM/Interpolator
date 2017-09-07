71d5e7c444c255091340154d8c2b46213367332d
User <User@USER-PK>
2017-09-07 19:51:26 +0300

    222

---
+++ InterpSolution/MeetingPro/Graphs.cs
@@ -0,0 +1,59 @@
+﻿using Interpolator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingPro {
+    public class Graphs {
+        static readonly Lazy<Graphs> lazyInstance = new Lazy<Graphs>(LoadFromFile);
+        static Graphs LoadFromFile() {
+            throw new NotImplementedException();
+        }
+        public static Graphs Instance => lazyInstance.Value;
+        public static Graphs GetNew => Instance.Copy();
+        protected Graphs(List<IInterpElem> list) {
+            throw new NotImplementedException();
+        }
+
+        protected Graphs(Graphs copyFrom) {
+            foreach (var gr in copyFrom.lstGr) {
+                lstGr.Add((IInterpElem)gr.Clone());
+            }
+            
+            foreach (var kv in copyFrom.dictGr) {
+                dictGr.Add(kv.Key, (IInterpElem)kv.Value.Clone());
+            }
+        }
+
+        public Graphs Copy() {
+            return new Graphs(this);
+        }
+
+        private static string filePath;
+        public static string FilePath {
+            get { return filePath; }
+            set { filePath = value; }
+        }
+
+        List<IInterpElem> lstGr = new List<IInterpElem>(30);
+        Dictionary<string, IInterpElem> dictGr = new Dictionary<string, IInterpElem>(30);
+
+        public IInterpElem this[string grName] {
+            get {
+                return dictGr[grName];
+            }
+        }
+
+        public IInterpElem this[int grNum] {
+            get {
+                return lstGr[grNum];
+            }
+        }
+
+        public int Count => lstGr.Count;
+
+        public string[] Names => dictGr.Keys.ToArray();
+    }
+}
--- InterpSolution/MeetingPro/Mis.cs
+++ InterpSolution/MeetingPro/Mis.cs
@@ -1,4 +1,5 @@
 ﻿using Interpolator;
+using Sharp3D.Math.Core;
 using SimpleIntegrator;
 using System;
 using System.Collections.Generic;
@@ -7,9 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MeetingPro {
-    public class Mis: MaterialObjectNewton {
-        public Dictionary<string, Interp2D> Graphs { get; set; }
+    public class Mis: ScnObjDummy {
+        public MaterialObjectNewton body;
+        Force Pforce;
+        public MisOptions opts;
+        public Mis(MisOptions opts) :base(opts.name) {
+            this.opts = opts;
+        }
+        public void Init() {
+            body = new MaterialObjectNewton("body");
+            Pforce = Force.GetForceCentered(0, new Vector3D(-1, 0, 0), body);
+            body.AddForce(Pforce);
+            
+        }
 
-        
+        public void SyncAct(double t) {
+            var deltam_rd = opts.graphs["deltam_rd"].GetV(opts.Temperature, t);
+            var deltam_md = opts.graphs["deltam_md"].GetV(opts.Temperature, t);
+            var x_m = opts.graphs["x_m"].GetV(deltam_rd, deltam_md);
+            var m = opts.graphs["m"].GetV(deltam_rd, deltam_md);
+            var i_x = opts.graphs["i_x"].GetV(deltam_rd, deltam_md);
+            var i_yz = opts.graphs["i_yz"].GetV(deltam_rd, deltam_md);
+
+            var r_rd = 9.80665 * opts.graphs["r_rd"].GetV(opts.Temperature, t);
+            var r_md = 9.80665 * opts.graphs["r_md"].GetV(opts.Temperature, t);
+            var alpha = GetAlpha();
+            var mach = GetMach();
+            var c_x = opts.graphs["c_x"].GetV(alpha, mach);
+            var c_r_x
+        }
+
+        public double GetAlpha() {
+            throw new NotImplementedException();
+        }
+
+        public double GetMach() {
+            throw new NotImplementedException();
+        }
     }
+
+    public class MisOptions {
+        public Graphs graphs;
+        public string name;
+        public int id;
+        public double Temperature;
+    }
+
+    
 }
