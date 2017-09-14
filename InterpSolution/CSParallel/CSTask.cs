2d6cbb1bd7ba2c159923388f02a293e2b8054768
User <User@USER-PK>
2017-09-14 23:56:05 +0300

    www

---
+++ InterpSolution/CSParallel/CSTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSParallel
+{
+    public abstract class CSTask<T>
+    {
+        public CSTask(CSExecutor executor) {
+            Executor = executor;
+        }
+
+        public CSExecutor Executor { get; }
+
+        public abstract void SolvAction(T initCond);
+
+    }
+}
---
+++ InterpSolution/CSParallel/IExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSParallel {
+    public interface IExecutor<TTask,TResult> {
+        IEnumerable<TResult> Run(IEnumerable<TTask> tasksInfos);
+        void StopAll();
+
+    }
+}
