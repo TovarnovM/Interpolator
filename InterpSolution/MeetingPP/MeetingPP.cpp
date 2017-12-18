4180a7de82d4f566eb59b9e073d1b242c86e2a28
User <User@USER-PK>
2017-12-19 02:16:06 +0300

    doney)

--- InterpSolution/MeetingPP/MeetingPP.cpp
+++ InterpSolution/MeetingPP/MeetingPP.cpp
@@ -11,26 +11,31 @@ using namespace std;
 int main()
 {
 	GrammyCluster cl;
-	cout <<endl<< cl.n_elems << endl << endl;
 	//cout << endl << cl.load_from_csv("C:\\mdata.csv") << endl << endl;
 	cout << endl << cl.load_from_bin("C:\\mdata.bin") << endl << endl;
-	
-	size_t *counts;
-	size_t count;
-	Vec3** traects;
+	for (size_t kk = 0; kk < 100; kk++)
+	{
+		size_t *counts;
+		size_t count;
+		Vec3** traects;
 
-	bool good = cl.GetExtrimeTraects(Vec3(0, 300, 0), Vec3(1, 0, 0), Vec3(6000, 0, 10), 30, &traects, count, &counts);
-	if (good) {
-		for (size_t i = 0; i < count; i++)
-		{
-			cout << "Traect "<<i<<"; count = "<<counts[i]<<endl;
-			for (size_t j = 0; j < counts[i]; j++)
+		bool good = cl.GetExtrimeTraects(Vec3(0, 300, 0), Vec3(1, 0, 0), Vec3(6000, 0, 10), 30, &traects, count, &counts);
+		if (good) {
+			for (size_t i = 0; i < count; i++)
 			{
-				cout << "(" << traects[i][j].x << "; " << traects[i][j].y << "; " << traects[i][j].z << ")" << endl;
+				cout << "Traect "<<i<<"; count = "<<counts[i]<<endl;
+				//for (size_t j = 0; j < counts[i]; j+=50)
+				//{
+				//	cout << "(" << traects[i][j].x << "; " << traects[i][j].y << "; " << traects[i][j].z << ")" << endl;
+				//}
+				cout <<"============" << kk << endl;
+				delete[] traects[i];
 			}
-			cout << endl;
+			delete[] traects;
+			delete[] counts;
 		}
 	}
+
 
 	//auto traect = cl.TraectToPoint(Vec3(0, 300, 0), Vec3(1, 0, 0), Vec3(6000, 0, 10), 30, count);
 	//for (size_t i = 0; i < count; i++)
